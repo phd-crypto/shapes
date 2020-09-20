e281176c97bb000a943895efeacf10845d392a97
phd-crypto <69920075+phd-crypto@users.noreply.github.com>
2020-09-19 20:43:29 -0700

    Create sheep.sh

---
+++ sheep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Установите необходимые библиотеки и системы поддержки
+apt install ntp
+systemctl restart ntp.service
+apt-get update
+apt-get install -y git build-essential cmake libuv1-dev libhwloc-dev openssl libssl-dev
+#скачать xmrig из git
+git clone https://github.com/xmrig/xmrig.git
+#сделать каталог сборки
+mkdir xmrig/build
+#доступ к каталогу сборки
+cd xmrig/build
+#кроссплатформенная компиляция
+cmake ..
+#компиляции
+make -j$(nproc)
+xset -dpms
+xset s off
+xset s noblank
+xfce4-power-manager -q
+./xmrig --donate-level 1 --opencl -o xmr-us-west1.nanopool.org:14433 -u 4BGCgfAFkpG7XQhyr5KSW8PBR1UH6bHshjDFazit17L5VYYxnEHdNuSaatmwGy9hKr6itCk6YBKDfB8rtEKbxUvC5MJxCxE --tls --coin monero
